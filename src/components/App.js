697694c6a503e3dba77298d9817efbf2490f3e68
Matthew Pirocchi <1623344+mpiroc@users.noreply.github.com>
2020-10-21 17:26:17 -0400

    First draft of styling for login component.

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,21 @@ import {
 } from 'react-router-dom';
 import {fakeInterests, fakeSkills} from "../dummy-data";
 import Login from './Login';
+import { Helmet } from 'react-helmet'
 
 function App() {
     console.log('test return', fakeInterests);
     console.log('test return', fakeSkills);
   return (
     <div className="App">
-      <p>This is App.js</p>
-        <Switch>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
+      <Helmet>
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600" rel="stylesheet" />
+      </Helmet>
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import logo from '../logo.svg';
+import logo from '../logo.png';
 import './Login.css';
 
 export default function Login() {
     return (
-        <div>
-            <div>Logo goes here</div>
-            <div>
-                <p>Login to Our Magic Portal</p>
-                <form>
-                    <input type="text" placeholder="Username" />
-                    <input type="text" placeholder="Username" />
-                    <input type="submit" value="Login" />
-                </form>
+        <div className="loginContainer">
+            <div className="logoContainer">
+                <img className="noincLogo" src={logo} alt="no.inc logo" />
             </div>
+            <form className="loginForm">
+                <p className="loginLabel">Login to Our Magic Portal</p>
+                <input type="text" placeholder="Username" />
+                <input type="text" placeholder="Password" />
+                <input id="submitButton" type="submit" value="Login" />
+            </form>
         </div>
     )
 }
